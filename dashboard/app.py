27293204b1e6f1ace1cf9140554aa10e2ff664b5
gaddampranav <gaddampranav7@gmail.com>
2026-03-01 23:32:35 +0530

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,18 +1,11 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2867
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+import streamlit as st
+import pandas as pd
 
-\f0\fs24 \cf0 import streamlit as st\
-import pandas as pd\
-\
-df = pd.read_csv("../data/engineered_student_data.csv")\
-\
-st.title("Student Burnout & Dropout Dashboard")\
-\
-st.metric("Average Risk Score", round(df["risk_score"].mean(), 2))\
-st.metric("High Risk %", round((df["risk_score"] > 70).mean()*100, 2))\
-\
-st.bar_chart(df["risk_score"].value_counts())}
+df = pd.read_csv("../data/engineered_student_data.csv")
+
+st.title("Student Burnout & Dropout Dashboard")
+
+st.metric("Average Risk Score", round(df["risk_score"].mean(), 2))
+st.metric("High Risk %", round((df["risk_score"] > 70).mean()*100, 2))
+
+st.bar_chart(df["risk_score"].value_counts())
